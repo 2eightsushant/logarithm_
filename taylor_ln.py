8aa21a5a1d6dffb06ab772abf530c4412769e384
Sushant Shrestha <2eight.sushant@gmail.com>
2024-05-04 10:13:39 +0545

    libraries were added for analysis

--- taylor_ln.py
+++ taylor_ln.py
@@ -1,4 +1,6 @@
 import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 def ln_taylor(x, it=50):
     f = 0
@@ -11,4 +13,11 @@ def ln_taylor(x, it=50):
         c = (-1)**n * n*abs(c)
     return f
     
- 
+x_values = np.linspace(1, 2.05, 20)  # Example x values
+y1 = [math.log(x) for x in x_values]  # Example y1 values
+y2 = [ln_taylor(x,5) for x in x_values]   # Example y2 values
+y3 = [ln_taylor(x,10) for x in x_values]
+y4 = [ln_taylor(x,30) for x in x_values]
+y5 = [ln_taylor(x,60) for x in x_values]
+y6 = [ln_taylor(x,80) for x in x_values]
+y7 = [ln_taylor(x,100) for x in x_values]
